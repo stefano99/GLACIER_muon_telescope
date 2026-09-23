01f68de717ad42aafd2ffd90abc26942e8f1abd7
stefano99 <falchi.stefi@hotmail.it>
2026-09-23 18:19:13 +0200

    fix: add timeouts for wificlient connection and mqtt keepalive to prevent 3min lock

--- arduino/src/publisher_mqtt.cpp
+++ arduino/src/publisher_mqtt.cpp
@@ -602,6 +602,12 @@ bool syncRTCirq() {
 
 // connect to MQTT broker
 bool connectToMQTT() {
+    // Force the underlying WiFi socket to time out after 5 seconds
+    wifiClient.setTimeout(5000);
+
+    // Set MQTT keepAlive to 15 seconds, cleanSession true, timeout 5000ms
+    mqtt.setOptions(15, true, 5000);
+
     // Connect to the MQTT broker
     mqtt.begin(MQTT_BROKER_ADRRESS, MQTT_PORT, wifiClient);
 
